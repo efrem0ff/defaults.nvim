1487b72fc1bf0ccdc5dcec4fc789a16c2a95bd55
efrem0ff <efremoff.sergey@gmail.com>
2024-06-26 14:31:55 +0300

    goimport func fixed

--- init.lua
+++ init.lua
@@ -279,32 +279,19 @@ vim.api.nvim_set_keymap("n", "<C-p>", ":cp<CR>", opts)
 vim.api.nvim_set_keymap("n", "<C-c>", ":ccl<CR>", opts)
 
 function goimports(timeoutms)
-  local context = { source = { organizeImports = true } }
+  local params = vim.lsp.util.make_range_params(nil, vim.lsp.util._get_offset_encoding())
+  params.context = { source = { organizeImports = true } }
   vim.validate { context = { context, "t", true } }
 
-  local params = vim.lsp.util.make_range_params()
-  params.context = context
-
-  -- See the implementation of the textDocument/codeAction callback
-  -- (lua/vim/lsp/handler.lua) for how to do this properly.
   local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, timeout_ms)
-  if not result or next(result) == nil then return end
-  local actions = result[1].result
-  if not actions then return end
-  local action = actions[1]
-
-  -- textDocument/codeAction can return either Command[] or CodeAction[]. If it
-  -- is a CodeAction, it can have either an edit, a command or both. Edits
-  -- should be executed first.
-  if action.edit or type(action.command) == "table" then
-    if action.edit then
-      vim.lsp.util.apply_workspace_edit(action.edit, vim.lsp.client().offset_encoding)
-    end
-    if type(action.command) == "table" then
-      vim.lsp.buf.execute_command(action.command)
-    end
-  else
-    vim.lsp.buf.execute_command(action)
+  for _, res in pairs(result or {}) do
+      for _, r in pairs(res.result or {}) do
+          if r.edit then
+              vim.lsp.util.apply_workspace_edit(r.edit, vim.lsp.util._get_offset_encoding())
+          else
+              vim.lsp.buf.execute_command(r.command)
+          end
+      end
   end
 end
 
@@ -325,6 +312,6 @@ nvim_create_augroups({
     {"BufWritePost", "init.lua", "PackerCompile"},
     {"TextYankPost", "*",  "silent! lua vim.highlight.on_yank()"},
     {"BufWritePre", "*.go", "lua vim.lsp.buf.format()"},
-    {"BufWritePre", "*.go", "lua goimports(1000)"},
+    {"BufWritePre", "*.go", "lua goimports(3000)"},
   },
 })
