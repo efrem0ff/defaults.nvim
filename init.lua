5a72a444137dc45a79f8c3d3711e038242de6014
efrem0ff <efremoff.sergey@gmail.com>
2024-09-02 18:09:17 +0300

    goimports compatibility changes. . .

--- init.lua
+++ init.lua
@@ -98,6 +98,8 @@ vim.api.nvim_set_keymap('n', 'j', "v:count == 0 ? 'gj' : 'j'", {noremap= true, e
 --Remap escape to leave terminal mode
 vim.api.nvim_set_keymap('t', '<Esc>', [[<c-\><c-n>]], {noremap = true})
 
+vim.api.nvim_create_user_command('E', 'Explore', {})
+
 --Map blankline
 vim.g.indent_blankline_char = "┊"
 vim.g.indent_blankline_filetype_exclude = { 'help', 'packer' }
@@ -260,6 +262,7 @@ nvim_lsp['gopls'].setup{
   capabilities = capabilities,
   settings = {
     gopls = {
+      buildFlags =  {"-tags=integration"},
       experimentalPostfixCompletions = true,
       analyses = {
         unusedparams = true,
@@ -280,19 +283,22 @@ vim.api.nvim_set_keymap("n", "<C-c>", ":ccl<CR>", opts)
 
 function goimports(timeoutms)
   local params = vim.lsp.util.make_range_params(nil, vim.lsp.util._get_offset_encoding())
-  params.context = { source = { organizeImports = true } }
-  vim.validate { context = { context, "t", true } }
-
-  local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, timeout_ms)
-  for _, res in pairs(result or {}) do
-      for _, r in pairs(res.result or {}) do
-          if r.edit then
-              vim.lsp.util.apply_workspace_edit(r.edit, vim.lsp.util._get_offset_encoding())
-          else
-              vim.lsp.buf.execute_command(r.command)
-          end
+  params.context = {only = {"source.organizeImports"}}
+  -- buf_request_sync defaults to a 1000ms timeout. Depending on your
+  -- machine and codebase, you may want longer. Add an additional
+  -- argument after params if you find that you have to write the file
+  -- twice for changes to be saved.
+  -- E.g., vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, 3000)
+  local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, timeoutms)
+  for cid, res in pairs(result or {}) do
+    for _, r in pairs(res.result or {}) do
+      if r.edit then
+        local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or "utf-16"
+        vim.lsp.util.apply_workspace_edit(r.edit, enc)
       end
+    end
   end
+  vim.lsp.buf.format({async = false})
 end
 
 function nvim_create_augroups(definitions)
